e16e82fdfcd04f938ec7671ac6be2df48be62168
VincentMugondora <vinmugondora@gmail.com>
2024-10-21 14:11:16 +0200

    updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
 import logo from "./images/logo6.PNG";
 import bannerImg from "./images/banner1.jpg";
-import projectOne from "./images/projects/projectOne.jpg";
+import projectOne from "./images/projects/project1.png";
 import projectTwo from "./images/projects/projectTwo.jpg";
 import projectThree from "./images/projects/projectThree.jpeg";
 import testimonialOne from "./images/testmonial/testimonialOne.png";
--- src/data/data.js
+++ src/data/data.js
@@ -1,42 +1,54 @@
 import { AiFillAppstore } from "react-icons/ai";
-import { FaMobile, FaGlobe} from "react-icons/fa";
-import { SiProgress, SiAntdesign } from "react-icons/si";
+import { FaMobile, FaProjectDiagram, FaCodeBranch } from "react-icons/fa";
+import { SiProgress, SiAntdesign, SiApollographql } from "react-icons/si";
 
 // Features Data
 export const featuresData = [
   {
     id: 1,
-    title: "Business Stratagy",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    title: "Business Strategy",
+    des: "Understanding client needs, defining goals, and crafting strategies for successful software projects.",
   },
   {
     id: 2,
     icon: <AiFillAppstore />,
     title: "App Development",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "Designing and building mobile and web applications with a focus on functionality and user experience.",
   },
   {
     id: 3,
     icon: <SiProgress />,
     title: "SEO Optimisation",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "Enhancing website visibility in search engines by optimizing code, content, and performance.",
   },
   {
     id: 4,
     icon: <FaMobile />,
     title: "Mobile Development",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "Building responsive and high-performing mobile applications for Android and iOS platforms.",
   },
   {
     id: 5,
     icon: <SiAntdesign />,
-    title: "UX Design",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    title: "UX/UI Design",
+    des: "Creating intuitive and user-friendly interfaces that improve user experience across digital products.",
   },
   {
     id: 6,
-    icon: <FaGlobe />,
-    title: "Hosting Websites",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    icon: <FaCodeBranch />,
+    title: "Version Control",
+    des: "Managing and collaborating on code using Git, ensuring efficient development workflows and code tracking.",
+  },
+  {
+    id: 7,
+    icon: <SiApollographql />,
+    title: "API Development",
+    des: "Building robust APIs to enable communication between different software components and platforms.",
+  },
+  {
+    id: 8,
+    icon: <FaProjectDiagram />,
+    title: "Project Management",
+    des: "Planning, organizing, and managing resources to bring about the successful completion of specific project goals.",
   },
 ];
